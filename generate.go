be856694d2a99f85a33d69f5d14239ff4f43a105
Giorgio Azzinnaro <giorgio@azzinna.ro>
2022-11-21 19:45:48 +0100

    refactor: use latest oapi-codegen config

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest -o api/farmfa.gen.go -generate types,client,server,spec -package api api/farmfa.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest -config api/config.yaml api/farmfa.yaml
